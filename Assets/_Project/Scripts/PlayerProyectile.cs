4eda99e11328fa8841b7bf1533f7531cc8fbe727
Ferchus <98918903+FerchusGames@users.noreply.github.com>
2024-03-05 19:02:55 -0600

    Initial commit

---
+++ Assets/_Project/Scripts/PlayerProyectile.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using FishNet.Example.Scened;
+using FishNet.Object;
+using UnityEngine;
+
+public class PlayerProyectile : NetworkBehaviour
+{
+    [SerializeField] private float _lifetime = 5;
+
+    [SerializeField] private int _damageAmount = 1;
+    
+    // [Client] // Only executed if it is a client
+    [Server] // Only executed if it is a server
+    private void Start()
+    {
+        Invoke(nameof(DestroyNetwork), _lifetime);
+    }
+
+    private void DestroyNetwork()
+    {
+        // Despawn only works with 'NetworkObject'
+        
+        Despawn(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && IsServer)
+        {
+            other.GetComponent<CharacterController>().TakeDamage(_damageAmount);
+            Despawn(gameObject);
+        }
+    }
+}
